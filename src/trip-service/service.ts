9c4c6300a85fe64c5dee6079e7e8faa2fec46838
Debkanchan Samadder <debu.samadder@gmail.com>
2023-05-19 16:55:03 +0530

    feat: add verification method

--- src/trip-service/service.ts
+++ src/trip-service/service.ts
@@ -3,6 +3,7 @@ import { TripService } from "../gen/ride/trip/v1alpha1/trip_service_connect.js";
 import verifyAuthHeader from "../utils/verify-auth-header.js";
 import createTrip from "./create-trip.js";
 import getTrip from "./get-trip.js";
+import startTripVerification from "./start-trip-verification.js";
 import startTrip from "./start-trip.js";
 
 const routes = (router: ConnectRouter) =>
@@ -18,6 +19,10 @@ const routes = (router: ConnectRouter) =>
 		startTrip: async (req, context) => {
 			await verifyAuthHeader(context);
 			return startTrip(req, context);
+		},
+		startTripVerification: async (req, context) => {
+			await verifyAuthHeader(context);
+			return startTripVerification(req, context);
 		},
 	});
 
