aa5519029906731dfaedd3630bc3edf6bd9cfcdb
Debkanchan Samadder <debu.samadder@gmail.com>
2023-07-15 17:53:38 +0530

    chore: update error messages

--- src/utils/verify-auth-header.ts
+++ src/utils/verify-auth-header.ts
@@ -4,17 +4,17 @@ import { verifyIdToken } from "../repositories/auth-repository.js";
 const verifyAuthHeader = async (context: HandlerContext): Promise<string> => {
 	try {
 		if (context.requestHeader.get("authorization").length === 0) {
-			throw new ConnectError("Missing Authorization", Code.Unauthenticated);
+			throw new ConnectError("missing authorization", Code.Unauthenticated);
 		}
 		const token = context.requestHeader.get("authorization").toString();
 
 		if (!token.startsWith("Bearer ")) {
-			throw new ConnectError("Invalid Authorization", Code.Unauthenticated);
+			throw new ConnectError("invalid token format", Code.Unauthenticated);
 		}
 
 		return await verifyIdToken(token.split("Bearer ")[1]);
 	} catch (e) {
-		throw new ConnectError("Invalid Authorization", Code.Unauthenticated);
+		throw new ConnectError("invalid authorization", Code.Unauthenticated);
 	}
 };
 
