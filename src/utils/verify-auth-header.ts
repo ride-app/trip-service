0e727d65d74c2791aba3cb7276e166d910cee7cb
Debkanchan Samadder <debu.samadder@gmail.com>
2023-07-20 19:50:01 +0530

    refactor verifyAuthHeader

--- src/utils/verify-auth-header.ts
+++ src/utils/verify-auth-header.ts
@@ -1,19 +1,31 @@
 import { Code, ConnectError, type HandlerContext } from "@bufbuild/connect";
 import { verifyIdToken } from "../repositories/auth-repository.js";
+import { logDebug, logError, logInfo } from "./logger.js";
 
-const verifyAuthHeader = async (context: HandlerContext): Promise<string> => {
+const verifyAuthHeader = async (context: HandlerContext): Promise<void> => {
 	try {
+		const token = context.requestHeader.get("authorization").toString();
+		logDebug("Token is: " + token);
+
 		if (context.requestHeader.get("authorization") === null) {
+			logInfo("Missing authorization");
 			throw new ConnectError("missing authorization", Code.Unauthenticated);
 		}
-		const token = context.requestHeader.get("authorization").toString();
 
 		if (!token.startsWith("Bearer ")) {
+			logInfo("Invalid token format");
 			throw new ConnectError("invalid token format", Code.Unauthenticated);
 		}
 
-		return await verifyIdToken(token.split("Bearer ")[1]);
+		logInfo("Verifying token");
+		const uid = await verifyIdToken(token.split("Bearer ")[1]);
+
+		logDebug("UID from header is: " + uid);
+
+		context.requestHeader.set("uid", uid);
 	} catch (e) {
+		logInfo("Invalid authorization");
+		logError(e);
 		throw new ConnectError("invalid authorization", Code.Unauthenticated);
 	}
 };
